2edbb3b32ddcf296413770e81b086eeb61cd8418
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-22 10:57:05 +0700

    Pembuatan file registrasi.php yang digunakan oleh user yang belum terdaftar

---
+++ register.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>LAPER - Registrasi</title>
+    </head>
+    <body>
+        <h2>Buat Akun Baru</h2>
+        <form method="post">
+            <!-- Tempat input Username -->
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Username">
+            <br>
+
+            <!-- Tempat input Email -->
+            <label for="email">Email</label>
+            <input type="Email" id="email" name="email" placeholder="Email">
+            <br>
+
+            <!-- Tempat input Password -->
+            <label for="password">Password</label>
+            <input type="Password" id="password" name="password" placeholder="Password">
+            <br>
+            
+            <!-- Button Sign-up -->
+            <input type="submit" value="Sign-up">
+            <br>
+        </form>
+    </body>
+</html>
