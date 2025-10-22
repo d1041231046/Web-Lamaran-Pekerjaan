6987cfa95ec22f78d6ed7daf46c44fa3f44d5cc6
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-22 11:39:50 +0700

    Pembuatan frame untuk tampilan halaman utama

---
+++ frame.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>LAPER - Beranda</title>
+    </head>
+    <body>
+        <iframe src="header.php" width="100%" height="15%" scrolling="no"></iframe>
+        <iframe src="main.php" width="100%" height="70%"></iframe>
+        <iframe src="footer.php" width="100%" height="15%" scrolling="no"></iframe>
+    </body>
+</html>
