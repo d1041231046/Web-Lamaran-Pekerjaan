f31b8a1da2b097165a3ba80cd6b3583c287cc748
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-22 17:18:59 +0700

    Memberi nama pada tiap frame

--- frame.php
+++ frame.php
@@ -3,8 +3,8 @@
         <title>LAPER - Beranda</title>
     </head>
     <body>
-        <iframe src="header.php" width="100%" height="15%" scrolling="no"></iframe>
-        <iframe src="main.php" width="100%" height="70%"></iframe>
-        <iframe src="footer.php" width="100%" height="15%" scrolling="no"></iframe>
+        <iframe name="header" src="header.php" width="100%" height="15%" scrolling="no"></iframe>
+        <iframe name="main" src="beranda.php" width="100%" height="70%"></iframe>
+        <iframe name="footer" src="footer.php" width="100%" height="15%" scrolling="no"></iframe>
     </body>
 </html>
