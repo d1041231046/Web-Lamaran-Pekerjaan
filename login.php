a44b80503a734e03286ccabe9d3154e3209d219b
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-22 10:10:21 +0700

    Penambahan kolom input bagian email

--- login.php
+++ login.php
@@ -4,5 +4,10 @@
     </head>
     <body>
         <h2>Login LAPER</h2>
+        <form method="post">
+            <!-- Tempat input Email -->
+            <label for="email">Email</label>
+            <input type="Email" id="email" name="email" placeholder="Email">
+        </form>
     </body>
 </html>
