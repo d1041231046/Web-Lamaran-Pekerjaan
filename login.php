52a6eb5e438a5b00eb00eac9f35d6c73bc74228c
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-22 10:12:46 +0700

    Penambahan kolom input bagian password

--- login.php
+++ login.php
@@ -8,6 +8,11 @@
             <!-- Tempat input Email -->
             <label for="email">Email</label>
             <input type="Email" id="email" name="email" placeholder="Email">
+            <br>
+
+            <!-- Tempat input Password -->
+            <label for="password">Password</label>
+            <input type="Password" id="password" name="password" placeholder="Password">
         </form>
     </body>
 </html>
