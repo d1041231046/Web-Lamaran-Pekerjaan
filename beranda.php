7f97ec2b33234c9e2a29a386a11083813009ad1a
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-22 21:27:16 +0700

    Perancangan isi beranda secara kasar

---
+++ beranda.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <link rel="icon" href="Logo.png">
+        <title>Beranda</title>
+    </head>
+    <body>
+        <!-- Slogan & Ajakan -->
+        <h1>&quot;LAPER &minus; Satu Langkah Menuju Pekerjaan Impian.&quot;</h1>
+        <button>Lihat Lowongan</button>
+
+        <!-- Tentang Website -->
+        <h2>
+            LAPER adalah platform sederhana yang membantu pencari kerja menemukan peluang pekerjaan dari berbagai perusahaan. 
+            Website ini juga memudahkan perusahaan untuk menerima dan memproses lamaran secara online.
+        </h2>
+
+        <!-- Testimoni -->
+         <div class="testimoni">
+            <p>
+                &quot;Dengan LAPER, saya bisa menemukan pekerjaan pertama saya dengan mudah!&quot;<br>
+                &minus; Rina, Web Developer
+            </p>
+         </div>
+
+         <div class="testimoni">
+            <p>
+                &quot;LAPER membantu saya menemukan pekerjaan yang sesuai dengan jurusan saya. 
+                Proses lamaran sangat mudah dan cepat.&quot;
+                &minus; Rizky A., Fresh Graduate Teknik Informatika
+            </p>
+         </div>
+
+         <div class="testimoni">
+            <p>
+                &quot;Saya tidak menyangka bisa diterima kerja hanya seminggu setelah melamar lewat LAPER. 
+                Web-nya sangat informatif dan mudah digunakan.&quot;
+                &minus; Maya P., Staff Administrasi
+            </p>
+         </div>
+    </body>
+</html>
