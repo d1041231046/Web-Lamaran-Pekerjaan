0b2808f4053a30801f3cdb0d19cb8a125e2b5098
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-22 21:50:52 +0700

    Pembuatan footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="footer.css">
+    </head>
+    <body>
+        <p><strong>LAPER</strong> &minus; Lapangan Pekerjaan</p>
+        <p>© 2025 LAPER. Semua hak dilindungi.</p>
+    </body>
+</html>
