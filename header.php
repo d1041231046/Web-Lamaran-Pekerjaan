ed333fa0fc9c75e4e45c5f5f0dce2edb2f7612d7
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-22 12:25:36 +0700

    Pembuatan struktur awal header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="header.css">
+    </head>
+    <body>
+        <!-- Bagian Logo -->
+        <div class="logo">
+            <img src="">
+        </div>
+
+        <!-- Navigation Bar -->
+        <div class="menu">
+            <a href="beranda.php">BERANDA</a>
+            <a href="lowongan.php">CARI LOWONGAN</a>
+        </div>
+    </body>
+</html>
