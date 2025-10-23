30d6fe0d385574eb1a351d62175cf1c61d4de81d
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-23 15:30:34 +0700

    penyesuaian ukuran header

--- frame.php
+++ frame.php
@@ -3,8 +3,8 @@
         <title>LAPER - Beranda</title>
     </head>
     <body>
-        <iframe name="header" src="header.php" width="100%" height="15%" scrolling="no"></iframe>
-        <iframe name="main" src="beranda.php" width="100%" height="75%"></iframe>
+        <iframe name="header" src="header.php" width="100%" height="10%" scrolling="no"></iframe>
+        <iframe name="main" src="beranda.php" width="100%" height="80%"></iframe>
         <iframe name="footer" src="footer.php" width="100%" height="10%" scrolling="no"></iframe>
     </body>
 </html>
