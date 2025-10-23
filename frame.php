2ffeb1f3efcbf6801ab18efcaaeb2f0f4e8300e9
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-23 15:22:40 +0700

    penyesuaian ukuran frame

--- frame.php
+++ frame.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <iframe name="header" src="header.php" width="100%" height="15%" scrolling="no"></iframe>
-        <iframe name="main" src="beranda.php" width="100%" height="70%"></iframe>
-        <iframe name="footer" src="footer.php" width="100%" height="15%" scrolling="no"></iframe>
+        <iframe name="main" src="beranda.php" width="100%" height="75%"></iframe>
+        <iframe name="footer" src="footer.php" width="100%" height="10%" scrolling="no"></iframe>
     </body>
 </html>
