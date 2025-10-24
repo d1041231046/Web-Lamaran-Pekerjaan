7ebb7c22de5df68515d427901bf2426c42fcfc8b
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-24 18:41:15 +0700

    Memperbarui struktur beranda dan stylingnya. Selain itu juga menambahkan foto example untuk bagian beranda

--- beranda.php
+++ beranda.php
@@ -2,40 +2,56 @@
     <head>
         <link rel="icon" href="Logo.png">
         <title>Beranda</title>
+        <link rel="stylesheet" href="beranda.css">
+        <script src="include.js" defer></script>
     </head>
     <body>
-        <!-- Slogan & Ajakan -->
-        <h1>&quot;LAPER &minus; Satu Langkah Menuju Pekerjaan Impian.&quot;</h1>
-        <button>Lihat Lowongan</button>
+        <header></header>
+        <main>
+            <!-- Slogan & Ajakan -->
+            <div class="utama">
+                <div class="utama-text">
+                    <h1>&quot;LAPER &minus; Satu Langkah Menuju Pekerjaan Impian.&quot;</h1>
+                    <button>Lihat Lowongan</button>
+                </div>
+                
+                <div class="utama-img">
+                    <img src="Chisa_Card.jpg">
+                </div>
+            </div>
 
-        <!-- Tentang Website -->
-        <h2>
-            LAPER adalah platform sederhana yang membantu pencari kerja menemukan peluang pekerjaan dari berbagai perusahaan. 
-            Website ini juga memudahkan perusahaan untuk menerima dan memproses lamaran secara online.
-        </h2>
+            <!-- Tentang Website -->
+            <div class="tentang">
+                <h2>
+                    LAPER adalah platform sederhana yang membantu pencari kerja menemukan peluang pekerjaan dari berbagai perusahaan. 
+                    Website ini juga memudahkan perusahaan untuk menerima dan memproses lamaran secara online.
+                </h2>
+            </div>
 
-        <!-- Testimoni -->
-         <div class="testimoni">
+            <!-- Testimoni -->
+            <div class="testimoni">
             <p>
                 &quot;Dengan LAPER, saya bisa menemukan pekerjaan pertama saya dengan mudah!&quot;<br>
                 &minus; Rina, Web Developer
             </p>
-         </div>
+            </div>
 
-         <div class="testimoni">
+            <div class="testimoni">
             <p>
                 &quot;LAPER membantu saya menemukan pekerjaan yang sesuai dengan jurusan saya. 
                 Proses lamaran sangat mudah dan cepat.&quot;
                 &minus; Rizky A., Fresh Graduate Teknik Informatika
             </p>
-         </div>
+            </div>
 
-         <div class="testimoni">
+            <div class="testimoni">
             <p>
                 &quot;Saya tidak menyangka bisa diterima kerja hanya seminggu setelah melamar lewat LAPER. 
                 Web-nya sangat informatif dan mudah digunakan.&quot;
                 &minus; Maya P., Staff Administrasi
             </p>
-         </div>
+            </div>
+        </main>
+        <footer></footer>
     </body>
 </html>
