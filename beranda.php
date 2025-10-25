3cf2abf38139ee960223637d753cf38e52a0cdc0
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-25 22:35:47 +0700

    Memanggil header dan footer dengan include serta menambah pengecekan session

--- beranda.php
+++ beranda.php
@@ -1,13 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php?error=beranda");
+    exit;
+}
+?>
+
 <html>
     <head>
         <link rel="icon" href="Logo.png">
-        <title>Beranda</title>
+        <title>LAPER - Beranda</title>
         <link rel="stylesheet" href="beranda.css">
-        <script src="include.js" defer></script>
+        <link rel="stylesheet" href="header.css">
+        <link rel="stylesheet" href="footer.css">
     </head>
     <body>
-        <header></header>
-        <main>
+        <?php include 'header.php'; ?>
+        <main class="beranda">
             <!-- Slogan & Ajakan -->
             <div class="utama">
                 <div class="utama-text">
@@ -29,29 +38,29 @@
             </div>
 
             <!-- Testimoni -->
-            <div class="testimoni">
-            <p>
-                &quot;Dengan LAPER, saya bisa menemukan pekerjaan pertama saya dengan mudah!&quot;<br>
-                &minus; Rina, Web Developer
-            </p>
-            </div>
-
-            <div class="testimoni">
-            <p>
-                &quot;LAPER membantu saya menemukan pekerjaan yang sesuai dengan jurusan saya. 
-                Proses lamaran sangat mudah dan cepat.&quot;
-                &minus; Rizky A., Fresh Graduate Teknik Informatika
-            </p>
-            </div>
-
-            <div class="testimoni">
-            <p>
-                &quot;Saya tidak menyangka bisa diterima kerja hanya seminggu setelah melamar lewat LAPER. 
-                Web-nya sangat informatif dan mudah digunakan.&quot;
-                &minus; Maya P., Staff Administrasi
-            </p>
+            <div class="testimoni-container">
+                <div class="testimoni-isi">
+                <p>
+                    &quot;Dengan LAPER, saya bisa menemukan pekerjaan pertama saya dengan mudah!&quot;<br>
+                    &minus; Rina, Web Developer
+                </p>
+                </div>
+                <div class="testimoni-isi">
+                <p>
+                    &quot;LAPER membantu saya menemukan pekerjaan yang sesuai dengan jurusan saya.
+                    Proses lamaran sangat mudah dan cepat.&quot;
+                    &minus; Rizky A., Fresh Graduate Teknik Informatika
+                </p>
+                </div>
+                <div class="testimoni-isi">
+                <p>
+                    &quot;Saya tidak menyangka bisa diterima kerja hanya seminggu setelah melamar lewat LAPER.
+                    Web-nya sangat informatif dan mudah digunakan.&quot;
+                    &minus; Maya P., Staff Administrasi
+                </p>
+                </div>
             </div>
         </main>
-        <footer></footer>
+        <?php include 'footer.php'; ?>
     </body>
 </html>
