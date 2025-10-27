fc156cb7b27ddae389a70f210844d7ede213e92a
Ferdyan <d1041231039@student.untan.ac.id>
2025-10-27 16:06:22 +0700

    Memperbarui halaman login beserta stylenya

--- login.php
+++ login.php
@@ -30,27 +30,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <html>
     <head>
+        <link rel="icon" href="Logo.png">
         <title>LAPER - Login</title>
+        <link rel="stylesheet" href="login.css">
     </head>
     <body>
-        <h2>Login LAPER</h2>
-        <form method="post" action="login_proses.php">
-            <!-- Tempat input Email -->
-            <label for="email">Email</label>
-            <input type="Email" id="email" name="email" placeholder="Email">
-            <br>
+        <div class="logo">
+            <img src="Logo.png">
+        </div>
+        <div class="login">
+            <h2>Login LAPER</h2>
+            <form method="post" action="login_proses.php">
+                <!-- Tempat input Email -->
+                <label class="label" for="email"><strong>Email</strong></label>
+                <input type="Email" id="email" name="email" placeholder="Email">
+                <br>
 
-            <!-- Tempat input Password -->
-            <label for="password">Password</label>
-            <input type="Password" id="password" name="password" placeholder="Password">
-            <br>
-            
-            <!-- Button Sign-in -->
-            <input type="submit" value="Sign-in">
-            <br>
+                <!-- Tempat input Password -->
+                <label class="label" for="password"><strong>Password</strong></label>
+                <input type="Password" id="password" name="password" placeholder="Password">
+                <br>
+                <br>
+                
+                <!-- Button Sign-in -->
+                <input type="submit" value="Sign-in" class="button">
+                <br>
 
-            <!-- Register account -->
-            <p>doesn't have an account? <a href="register.php">click here to sign up</a></p>
-        </form>
+                <!-- Register account -->
+                <p>doesn't have an account? <a href="register.php">click here to sign up</a></p>
+            </form>
+        </div>
     </body>
 </html>
